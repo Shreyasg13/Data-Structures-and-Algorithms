674055f20c99b3b63f4cc6f9550bcacdf1d25104
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-06-01 13:07:19 -0400

    Time: 98 ms (64.67%), Space: 14 MB (57.94%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,35 +1,17 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res=[]
-        # candidates.sort()
-        self.dfs(candidates,target,0,[],res)
+        self.backtrack(candidates,target,0,[],res)
         return res
-    def dfs(self,nums,target,index,path,res):
+        
+    def backtrack(self,candidates,target,index,path,res):
         if target < 0:
             return
-        if target==0:
+        if target == 0:
             res.append(path)
             return
-        for i in range(index,len(nums)):
-            self.dfs(nums,target-nums[i],i,path+[nums[i]],res)
         
+        for i in range(index,len(candidates)):
+            self.backtrack(candidates,target-candidates[i],i,path+[candidates[i]],res)
         
-        
-        
-                    
-#     def combine(self, n, k):
-#         res = []
-#         self.dfs(range(1,n+1), k, 0, [], res)
-#         return res
-    
-#     def dfs(self, nums, k, index, path, res):
-#         #if k < 0:  #backtracking
-#             #return 
-#         if k == 0:
-#             res.append(path)
-#             return # backtracking 
-#         for i in range(index, len(nums)):
-#             self.dfs(nums, k-1, i+1, path+[nums[i]], res)
-        
-    
-        
+       
