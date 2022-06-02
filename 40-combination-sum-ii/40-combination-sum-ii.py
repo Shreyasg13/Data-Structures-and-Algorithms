a2f007eb74df33370d1afff7ae04178bdec5ff82
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-06-02 01:10:07 -0400

    Time: 161 ms (18.16%), Space: 13.9 MB (93.24%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.py
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -22,22 +22,7 @@ class Solution:
             
             
             
-#             def combinationSum2(self, candidates, target):
-#     res = []
-#     candidates.sort()
-#     self.dfs(candidates, target, 0, [], res)
-#     return res
-    
-# def dfs(self, candidates, target, index, path, res):
-#     if target < 0:
-#         return  # backtracking
-#     if target == 0:
-#         res.append(path)
-#         return  # backtracking 
-#     for i in xrange(index, len(candidates)):
-#         if i > index and candidates[i] == candidates[i-1]:
-#             continue
-#         self.dfs(candidates, target-candidates[i], i+1, path+[candidates[i]], res)
+
             
         
 
