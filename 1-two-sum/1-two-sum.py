d686d2bd52061f8a941ebf42bb656e9a60e559ca
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-06-06 23:23:43 -0400

    Time: 143 ms (37.41%), Space: 15.2 MB (24.06%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,19 +1,12 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-       
-        vector<int> ans;
-        for(int i=0;i<nums.size();i++)
-        {
-            for(int j=i+1;j<nums.size();j++)
-                if(nums[i]+nums[j]==target)
-                {
-                    ans.push_back(i);
-                    ans.push_back(j);
-                }
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        Dict={}
+        for i in range (len(nums)):
+            num2=target-nums[i]
+            if  num2 not in Dict :
+                Dict[nums[i]]=i
+            else:
+                return[Dict[num2],i]
+                
             
-        }
         
-        return ans;
-    }
-};
