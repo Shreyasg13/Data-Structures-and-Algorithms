4a3c2d635fff250003e84b536df77e4455d870fa
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-06-06 20:56:31 -0400

    Time: 97 ms (16.61%), Space: 14.4 MB (66.02%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -7,8 +7,8 @@ class Solution:
         for i in range(len(s)):    
             D1[s[i]]=1+D1.get(s[i],0)
             D2[t[i]]=1+D2.get(t[i],0)
-        for ele in D1:
-            if D1[ele] != D2.get(ele,0):
+        for i in range(len(s)):
+            if D1[s[i]] != D2.get(s[i],0):
                     return False
         return True
                     
