2526c88b32e5154f4c3a08b558529a2d589640c3
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-06-13 17:35:01 -0400

    Time: 38 ms (81.68%), Space: 14 MB (95.74%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,19 +1,29 @@
 class Solution:
+#     def subsets(self, nums: List[int]) -> List[List[int]]:
+#         ans=[]
+#         subset=[]
+        
+#         def dfs(i):
+#             if i >= len(nums):
+#                 ans.append(subset.copy())
+#                 return
+            
+            
+#             subset.append(nums[i])
+#             dfs(i+1)
+#             subset.pop()
+#             dfs(i+1)
+        
+#         dfs(0)
+#         return ans
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans=[]
-        subset=[]
-        
-        def dfs(i):
-            if i >= len(nums):
-                ans.append(subset.copy())
-                return
+        res=[]
+        self.dfs(nums,[],0,res)
+        return res
+    
+    def dfs(self,nums,path,index,res):
+        res.append(path)
+        for i in range (index,len(nums)):
+            self.dfs(nums,path+[nums[i]],i+1,res)
             
-            
-            subset.append(nums[i])
-            dfs(i+1)
-            subset.pop()
-            dfs(i+1)
-        
-        dfs(0)
-        return ans
         
