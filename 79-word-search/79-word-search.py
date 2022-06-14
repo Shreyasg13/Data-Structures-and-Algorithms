39b26e2ed6da9dff1c661f4ea1ad951541bc05a7
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-06-14 02:42:45 -0400

    Time: 8267 ms (28.47%), Space: 14 MB (50.05%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,30 +1,44 @@
-class Solution:
-    # @param board, a list of lists of 1 length string
-    # @param word, a string
-    # @return a boolean
-    # 3:42
-    def exist(self, board, word):
-        visited = {}
-
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if self.getWords(board, word, i, j, visited):
-                    return True
+#     def exist(self, board: List[List[str]], word: str) -> bool:
+#         visited={}
+#         rows,cols=len(board),len(board[0])
+#         for r in range(rows):
+#             for c in range(cols):
+#                 if self.WordSearch(board,word,r,c,visited):
+#                     return True
+#         return False
+    
+#     def WordSearch(self,board,word,r,c,visited,index=0):
         
+#         if index==len(word):
+#             return True
+        
+#         if r < 0 or r == len(board) or c<0 or c == len(board[0]) or board[r][c] != word[index] or visited.get((r,c)) :
+#                 return False
+            
+#         visited[(r,c)]=True
+#         res=(self.WordSearch(board,word,r,c+1,visited,index+1) or self.WordSearch(board,word,r,c-1,visited,index+1) or
+#              self.WordSearch(board,word,r+1,c,visited,index+1) or self.WordSearch(board,word,r-1,c,visited,index+1) )
+#         visited[(r,c)]=False            
+            
+#         return res
+    
+    
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        
+        def dfs(row, column, index):
+            if index == len(word): return True
+            path.add((row, column))
+            for r, c in [(row-1, column), (row, column-1), (row, column+1), (row+1, column)]:
+                if r in range(M) and c in range(N) and board[r][c] == word[index] and (r, c) not in path:
+                    if dfs(r, c, index+1): return True
+            path.remove((row, column))
+        
+        M, N = len(board), len(board[0])
+        path = set()
+        for row in range(M):
+            for column in range(N):
+                if board[row][column] == word[0]:
+                    if dfs(row, column, 1): return True
         return False
-
-    def getWords(self, board, word, i, j, visited, pos = 0):
-        if pos == len(word):
-            return True
-
-        if i < 0 or i == len(board) or j < 0 or j == len(board[0]) or visited.get((i, j)) or word[pos] != board[i][j]:
-            return False
-
-        visited[(i, j)] = True
-        res = self.getWords(board, word, i, j + 1, visited, pos + 1) \
-                or self.getWords(board, word, i, j - 1, visited, pos + 1) \
-                or self.getWords(board, word, i + 1, j, visited, pos + 1) \
-                or self.getWords(board, word, i - 1, j, visited, pos + 1)
-        visited[(i, j)] = False
-
-        return res
+    
