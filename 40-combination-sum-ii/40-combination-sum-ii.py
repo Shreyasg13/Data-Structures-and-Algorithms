af0b52e5ee1a8fc4ddfcde393897d1661fde0a78
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-06-14 01:18:45 -0400

    Time: 153 ms (23.45%), Space: 13.8 MB (92.91%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.py
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -1,29 +1,18 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        ans=[]
+        res=[]
         candidates.sort()
-        self.dfs(candidates,target,0,[],ans)
-        return ans
+        self.dfs(candidates,target,[],0,res)
+        return res
     
-    def dfs(self,nums,target,index,path,ans):
+    def dfs(self,candidates,target,path,index,res):
+        
         if target < 0:
             return
-        if target==0:
-            ans.append(path)
-            return
-           
-        
-        for i in range (index,len(nums)):
-            if nums[i]==nums[i-1] and i>index:
+        if target==0 and path not in res:
+            res.append(path)
+            
+        for i in range(index,len(candidates)):
+            if candidates[i]==candidates[i-1] and i > index:
                 continue
-            self.dfs(nums,target-nums[i],i+1,path+[nums[i]],ans)
-            
-            
-            
-            
-            
-
-            
-        
-
-        
+            self.dfs(candidates,target-candidates[i],path+[candidates[i]],i+1,res)
