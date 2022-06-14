50b8c63dbcf8832a2c33a94a9e872e755d030b88
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-06-14 02:52:38 -0400

    Time: 1331 ms (97.48%), Space: 14 MB (50.05%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,54 +1,55 @@
-#     def exist(self, board: List[List[str]], word: str) -> bool:
-#         visited={}
-#         rows,cols=len(board),len(board[0])
-#         for r in range(rows):
-#             for c in range(cols):
-#                 if self.WordSearch(board,word,r,c,visited):
-#                     return True
-#         return False
-    
-#     def WordSearch(self,board,word,r,c,visited,index=0):
-        
-#         if index==len(word):
-#             return True
-        
-#         if r < 0 or r == len(board) or c<0 or c == len(board[0]) or board[r][c] != word[index] or visited.get((r,c)) :
-#                 return False
-            
-#         visited[(r,c)]=True
-#         res=(self.WordSearch(board,word,r,c+1,visited,index+1) or self.WordSearch(board,word,r,c-1,visited,index+1) or
-#              self.WordSearch(board,word,r+1,c,visited,index+1) or self.WordSearch(board,word,r-1,c,visited,index+1) )
-#         visited[(r,c)]=False            
-            
-#         return res
-    
-    
-def dfs(board, word, r, c, visited):
-    if not word:
-        return True
-
-    if (
-        (r, c) in visited
-        or r < 0
-        or r >= len(board)
-        or c < 0
-        or c >= len(board[0])
-        or board[r][c] != word[0]
-    ):
-        return False
-
-    return (
-        dfs(board, word[1:], r + 1, c, visited + [(r, c)])
-        or dfs(board, word[1:], r - 1, c, visited + [(r, c)])
-        or dfs(board, word[1:], r, c + 1, visited + [(r, c)])
-        or dfs(board, word[1:], r, c - 1, visited + [(r, c)])
-    )
-
+from collections import defaultdict, Counter
 
 class Solution:
-    def exist(self, board, word):
-        for r, row in enumerate(board):
-            for c, l in enumerate(row):
-                if dfs(board, word, r, c, []):
-                    return True
-        return False
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        
+        def nn(coord):
+            r, c = coord
+            return [(r, c+1), (r+1, c), (r, c-1), (r-1, c)]
+            
+        
+        letter_counts = Counter(word)
+        l2rc = defaultdict(set)  # letter to row coln
+        for rdx, row in enumerate(board):
+            for cdx, val in enumerate(row):
+                if val in letter_counts:
+                    l2rc[val].add((rdx, cdx))
+        
+        for val, count in letter_counts.items():
+            if len(l2rc[val]) < count:
+                return False
+        
+        def dfs(pt, prev=None, visited=None) -> bool:
+            
+            if pt == len(word):
+                return True
+
+            l = word[pt]
+            # core
+            coords = l2rc[l]
+            if prev is None:    
+                for rc in coords:
+                    visited = {rc}
+                    if dfs(pt + 1, rc, visited):
+                        return True
+                return False
+            
+            nns = nn(prev)
+            for rc in nns:
+                if rc in coords and rc not in visited:
+                    if dfs(pt + 1, rc, visited | {rc}):
+                        return True
+            return False
+        
+        
+        return dfs(0)
+
+            
+            
+                    
+                    
+            
+            
+            
+            
+            
