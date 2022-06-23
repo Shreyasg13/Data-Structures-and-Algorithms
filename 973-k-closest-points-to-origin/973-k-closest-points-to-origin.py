512a83ee09cd8828238d325b67cce6fd51a7e625
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-06-23 16:57:21 -0400

    Time: 953 ms (72.32%), Space: 20.3 MB (59.17%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -5,13 +5,11 @@ class Solution:
         for i in range(len(points)):
             print(i)
             sqrt.append([-1*(points[i][0]**2 + points[i][1]**2),i])
-              
             
         heapq.heapify(sqrt)
-        print(sqrt)
-
         while len(sqrt) > k :
-            heapq.heappop(sqrt)
+                heapq.heappop(sqrt)
+    
         
         for [x,y] in sqrt:
             res.append(points[y])
