e6261ded0eaf5d459d63459af270e9ef991ff237
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-06-24 15:26:30 -0400

    Time: 184 ms (27.61%), Space: 18.9 MB (33.76%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -2,10 +2,25 @@ import heapq
 from collections import Counter
 class Solution:
     def topKFrequent(self, nums, k):
-        res = []
-        dic = Counter(nums)
-        max_heap = [(-val, key) for key, val in dic.items()]
-        heapq.heapify(max_heap)
+        Map={}
+        MinHeap=[]
+        for ele in nums:
+            if ele not in Map:
+                Map[ele]=-1
+            else:
+                Map[ele]-=1
+        print(Map)
+        
+        for key,val in Map.items():
+            heapq.heappush(MinHeap,[val,key])
+        heapq.heapify(MinHeap)
+        print(MinHeap)
+        
+        res=[]
         for i in range(k):
-            res.append(heapq.heappop(max_heap)[1])
-        return res 
+            val,key=heapq.heappop(MinHeap)
+            res.append(key)
+            
+        return res
+            
+        
