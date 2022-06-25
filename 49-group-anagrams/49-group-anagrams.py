6b18d46a9e4c8613385fc06250da68c210321914
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-06-24 20:42:15 -0400

    Time: 112 ms (85.18%), Space: 18.5 MB (41.96%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,18 +1,6 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        Map=defaultdict(list)
-        
+        Map=defaultdict(list)  
         for word in strs:
-            Map[tuple(sorted(word))].append(word)
-        
+            Map[tuple(sorted(word))].append(word)     
         return Map.values()
-        
-#         for word in strs:
-#             hash=[0]*26
-#             for c in word:
-#                 hash[ord(c)-ord('a')]+=1
-#             Map[tuple(hash)].append(word)
-            
-#         # print(Map.keys(),"\n")
-#         # print(Map.values())
-#         return Map.values()
