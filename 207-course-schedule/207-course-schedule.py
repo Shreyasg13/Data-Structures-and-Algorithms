9a3da19d3c3aeb7c2e8abe8055bea467c7ec9fe6
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-07-06 21:59:51 -0400

    Time: 167 ms (40.16%), Space: 17.1 MB (39.37%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,24 +1,24 @@
-class Solution:  
-    
-    def canFinish(self, N, PreReq):
-        graph = {i:[] for i in range(N)}
-        status = [0 for i in range(N)]
-        for x,y in PreReq:
-            graph[x].append(y)
-        print(graph)
+class Solution:
+    def canFinish(self, N, edges):
+        Adj_List = [[] for _ in range(N)]
+        status = [0 for _ in range(N)]
+        for x,y in edges: Adj_List[x].append(y)
         
-        def dfs(i):
-            if status[i] == -1:
-                return False
-            if status[i] == 1:
+        def dfs(crs):
+            # if came across visited course
+            if status[crs]==1:
                 return True
+            # already visited course
+            if status[crs]==-1:
+                return False
+            status[crs]= -1
             
-            status[i]= -1
-            for j in graph[i]:    
-                if not dfs(j):
+            for pre in Adj_List[crs]:
+                
+                if not dfs(pre):
                     return False
             
-            status[i]= 1
+            status[crs]=1
             return True
         
         for crs in range(N):
