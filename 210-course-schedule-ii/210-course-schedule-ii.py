57f82974fabbf7990d85f52c0527a04c35888e02
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-07-06 23:19:26 -0400

    Time: 268 ms (8.08%), Space: 17.4 MB (26.70%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        pre={i:[] for i in range(numCourses)}
+        
+        for x,y in prerequisites:
+            pre[x].append(y)
+        cycle,taken=set(),[]
+        def CanTake(crs):
+            
+            if crs in cycle:
+                return False
+            if crs in taken:
+                return True  
+            
+            cycle.add(crs)
+            for ele in pre[crs]:
+                if CanTake(ele)==False:
+                    return False    
+            
+            cycle.remove(crs)
+            taken.append(crs)
+            return True
+            
+            
+        for i in range(numCourses):
+            if CanTake(i)==False:
+                return []
+                
+        return taken
+                
+        
