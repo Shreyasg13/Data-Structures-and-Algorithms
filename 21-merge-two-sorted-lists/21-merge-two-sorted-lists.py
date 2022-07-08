224996e936609f141e82a0a27f4b1a75ca12b831
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-07-08 15:49:25 -0400

    Time: 51 ms (64.57%), Space: 13.9 MB (79.02%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -7,31 +7,31 @@ class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         
         # Recursive
-        # if not list1 or not list2:
-        #     return list1 or list2
-        # 
-        # if list1.val < list2.val:
-        #     list1.next=self.mergeTwoLists(list1.next,list2)
-        #     return list1
-        # else:
-        #     list2.next=self.mergeTwoLists(list1,list2.next)
-        #     return list2
+        if not list1 or not list2:
+            return list1 or list2
+        
+        if list1.val < list2.val:
+            list1.next=self.mergeTwoLists(list1.next,list2)
+            return list1
+        else:
+            list2.next=self.mergeTwoLists(list1,list2.next)
+            return list2
           
             
-            # Iterative
-        dummy = res = ListNode()
-        # res.next=list1.val
-        while list1 and list2:    
-            if list1.val <list2.val:
-                res.next=list1
-                list1=list1.next
-            else:
-                res.next=list2
-                list2=list2.next
+#             # Iterative
+#         dummy = res = ListNode()
+#         # res.next=list1.val
+#         while list1 and list2:    
+#             if list1.val <list2.val:
+#                 res.next=list1
+#                 list1=list1.next
+#             else:
+#                 res.next=list2
+#                 list2=list2.next
                 
-            res=res.next
-        res.next=list1 or list2
-        return dummy.next
+#             res=res.next
+#         res.next=list1 or list2
+#         return dummy.next
         
             
             
