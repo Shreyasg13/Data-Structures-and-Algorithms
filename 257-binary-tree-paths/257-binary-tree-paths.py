b7337a5a0bd1cf4eaf67fbedbd5eabeb5d3aff36
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-07-13 02:05:57 -0400

    Time: 36 ms (88.22%), Space: 14 MB (28.63%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.py
+++ 257-binary-tree-paths/257-binary-tree-paths.py
@@ -13,47 +13,13 @@ class Solution:
     
     def dfs(self,root,path,res):
         
-       
+        # if left or right exist : recursing along with adding path
         if root.left:
             self.dfs(root.left,path+str(root.val)+"->",res)
         if root.right:
             self.dfs(root.right,path+str(root.val)+"->",res)
-        
+       # if end node end it and append the path 
         if not root.left and not root.right:
             res.append(path+str(root.val))
             return
                 
-
-            
-#              # dfs recursively
-#     def binaryTreePaths(self, root):
-#         if not root:
-#             return []
-#         res = []
-#         self.dfs(root, "", res)
-#         return res
-    
-#     def dfs(self, root, ls, res):
-#         if not root.left and not root.right:
-#             res.append(ls+str(root.val))
-#         if root.left:
-#             self.dfs(root.left, ls+str(root.val)+"->", res)
-#         if root.right:
-#             self.dfs(root.right, ls+str(root.val)+"->", res)
-            
-            
-            
-            
-        
-#     def dfs(self,root,path,res):
-#         path.append(root)
-    
-#         if not root:
-#             return
-#         if not root.left or not root.right:
-#             path+="->"
-#             self.dfs(root.left,path)
-            
-            
-            
-            
