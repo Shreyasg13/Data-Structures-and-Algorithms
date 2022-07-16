86f54d3f9226eec90bca9661996f66a79f50e2ae
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-07-16 02:51:28 -0400

    Time: 57 ms (31.13%), Space: 13.9 MB (24.40%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -1,7 +1,7 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         # finding max among 1 to n-1 and n to 1  and first element
-        return max( nums[0],self.rob_with_linear(nums[1:]) , self.rob_with_linear(nums[0:len(nums)-1]) )
+        return max( nums[0],self.rob_with_linear(nums[1:]) , self.rob_with_linear(nums[0:-1]) )
         
     def rob_with_linear(self,amount):
         fir,sec=0,0
