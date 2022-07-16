3c745326b9ecbc1afe6dbbbbfaba606e702c2200
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-07-16 04:04:04 -0400

    Time: 1019 ms (79.56%), Space: 13.9 MB (91.28%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,16 +1,18 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         longest=""
+        
         for i in range(len(s)):
+            # checking for odd pal seq
             tmp=self.Pallindrome(s,i,i)
             if len(tmp) > len(longest):
                 longest=tmp
-            
+            # checking for even pal seq
             tmp=self.Pallindrome(s,i,i+1)
             if len(tmp) > len(longest):
                 longest=tmp
         return longest
-            
+    # get longest pallindrome at index i       
     def Pallindrome(self,s,l,r):
         while l>=0 and r<=len(s)-1 and s[r]==s[l]:
             l-=1
