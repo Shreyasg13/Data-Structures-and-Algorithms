ff954da813cd63b443dc8267f627aea4c8e2e94d
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-07-15 21:23:21 -0400

    Time: 43 ms (58.44%), Space: 13.9 MB (11.65%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,7 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        # top down approach
         last,prev=1,1
+        
         for i in range(n-1):
+            # change the last to previous and simply keep updating previou \
             last,prev=prev,prev+last
         return prev
         
