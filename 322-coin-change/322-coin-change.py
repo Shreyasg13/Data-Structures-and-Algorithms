d9b2e9b172aa9adf57ba65814760e859cfe9f9ca
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-07-17 23:56:49 -0400

    Time: 3093 ms (15.09%), Space: 14.2 MB (55.54%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,6 +1,7 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [0] + [sys.maxsize for _ in range(amount)]
+        dp = [sys.maxsize for _ in range(amount+1)]
+        dp[0]=0
         for i in range(1, amount+1):
             for coin in coins:
                 if i - coin >= 0:
@@ -8,29 +9,4 @@ class Solution:
         if dp[amount] == sys.maxsize:
             return -1
         return dp[amount]
-            
-        
-        
-#         coins.sort()
-#         print(coins[len(coins)-1])
-#         if coins[len(coins)-1] > amount:
-#             return -1
-#         # amount=0
-#         count=0
-#         for i in range(len(coins)-1,-1,-1):
-#             if coins[i] > amount:
-#                 pass
-#             else:
-#                 count+=amount//coins[i]
-#                 amount%=coins[i]
-#                 print(count,amount)
-#             # if rem amount is less than coin
-#             if amount //coin[i]:
-#                 return -1
-#             if amount==0:
-#                 return count
-        
-            
-#         return count
-        
-        
+ 
