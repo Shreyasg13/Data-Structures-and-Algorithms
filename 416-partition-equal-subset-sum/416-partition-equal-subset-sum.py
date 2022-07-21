a8b9b8398e7a73a235942f5af08f6f8248a4e1c4
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-07-21 18:31:17 -0400

    Time: 922 ms (66.75%), Space: 15.5 MB (58.48%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -6,12 +6,29 @@ class Solution:
        
         dp=set()
         dp.add(0)
+        # keep on adding the possible combinations of sum 
         for i in range(len(nums)-1,-1,-1):
             new_dp=set()
             for ele in dp:
+                # also adding current element for not loosing the pre 
+                if ele==target or ele+nums[i]==target:
+                    return True
                 new_dp.add(ele)
                 new_dp.add(ele+nums[i])
             dp=new_dp
        
         
         return True if target in dp else False
+    
+    
+                    
+#     def dfs(self,candidates,target,index):
+        
+#         if target < 0:
+#             return
+#         elif target==0:
+#             return True
+#         for i in range(index,len(candidates)):
+#             self.dfs(candidates,target-candidates[i],i+1)       
+#         return False
+
