5a137a7638a8db8a621276d8a033a366a205c236
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-07-23 18:27:04 -0400

    Time: 659 ms (38.61%), Space: 25.3 MB (43.63%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.py
+++ 518-coin-change-2/518-coin-change-2.py
@@ -7,9 +7,10 @@ class Solution:
         
         for a in range(1,amount+1):
             for i in range (len(coins)-1,-1,-1):
-                # checking number of ways the coin can sum to amount
+            
+            # checking number of ways the coin can sum to amount 
                 dp[a][i]=dp[a][i+1]
                 if a-coins[i] >=0:
                     dp[a][i]+=dp[a-coins[i]][i]
-        print(dp)
+        # returning the ways we can sum upto amount
         return dp[amount][0]
