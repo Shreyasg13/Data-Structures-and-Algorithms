8e4853e95953ed78b0d719989efd286db8b26f4a
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-07-23 18:46:54 -0400

    Time: 350 ms (58.48%), Space: 14.4 MB (51.05%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.py
+++ 518-coin-change-2/518-coin-change-2.py
@@ -1,16 +1,15 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        # Intialize 2D array 
-        dp  =   [[0 for _ in range(len(coins)+1)] for _ in range(amount+1)]
-        dp[0] = [1] * (len(coins) + 1)
-        # Bottom up approach 
+        dp = [0] * (amount + 1)
+        dp[0] = 1
         
-        for a in range(1,amount+1):
-            for i in range (len(coins)-1,-1,-1):
-            
-            # checking number of ways the coin can sum to amount 
-                dp[a][i]=dp[a][i+1]
-                if a-coins[i] >=0:
-                    dp[a][i]+=dp[a-coins[i]][i]
-        # returning the ways we can sum upto amount
-        return dp[amount][0]
+        for i in range(len(coins) - 1, -1, -1):
+            nextDP = [0] * (amount + 1)
+            nextDP[0] = 1
+
+            for a in range(1, amount + 1):
+                nextDP[a] = dp[a]
+                if a - coins[i] >= 0:
+                    nextDP[a] += nextDP[a - coins[i]]
+            dp = nextDP
+        return dp[amount]
