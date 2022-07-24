0f3115abc254b55f940b2bb56c72a3dcc6d1e0a9
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-07-24 12:33:45 -0400

    Time: 363 ms (57.07%), Space: 14.3 MB (53.01%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.py
+++ 518-coin-change-2/518-coin-change-2.py
@@ -3,16 +3,10 @@ class Solution:
         dp = [0] * (amount + 1)
         dp[0] = 1
         
-        for i in range(len(coins) - 1, -1, -1):
-            # creating temporory memory
-            new_dp = [0] * (amount + 1)
-            new_dp[0] = 1
-
+        for coin in coins:
             for a in range(1, amount + 1):
-                new_dp[a] = dp[a]
                 # if amount is left or equals keep on adding 
-                if a - coins[i] >= 0:
-                    new_dp[a] += new_dp[a - coins[i]]
-            # re-assign values to dp
-            dp = new_dp
-        return dp[amount]
+                if a - coin >= 0:
+                    dp[a] += dp[a - coin]
+        print(dp)
+        return dp[-1]
