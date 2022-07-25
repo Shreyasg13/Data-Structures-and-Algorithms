6c823c4f489cba10ff227630cdd6d63f3dd0621f
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-07-24 23:25:55 -0400

    Time: 65 ms (25.82%), Space: 13.8 MB (74.54%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        rows,cols=len(matrix),len(matrix[0])
+        # Intervrting elements diagonally
+        for r in range(rows):
+            for c in range(r,cols):
+                matrix[r][c],matrix[c][r]=matrix[c][r],matrix[r][c]
+                
+            # reversing the rows
+            matrix[r]=matrix[r][::-1]
+                
+       
