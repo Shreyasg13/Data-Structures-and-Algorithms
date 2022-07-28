c02e2e8c8738694abb77a3960658a5efedbc8c90
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-07-28 15:10:21 -0400

    Time: 599 ms (23.30%), Space: 25.5 MB (9.25%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -4,7 +4,7 @@ class Solution:
         
         
         rows,cols=len(grid),len(grid[0])
-        pos=[(0,1),(0,-1),(1,0),(-1,0)]
+        # pos=[(0,1),(0,-1),(1,0),(-1,0)]
         visited=set()
         
         def dfs(r,c):
@@ -13,9 +13,12 @@ class Solution:
             or (r,c) in visited or grid[r][c]=='0'):
                     return
             visited.add((r,c))
-            for lr,lc in pos:
-                dfs(r+lr,c+lc)    
-                
+#             for lr,lc in pos:
+#                 dfs(r+lr,c+lc)    
+            dfs(r-1,c)
+            dfs(r+1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
                 
             
             
