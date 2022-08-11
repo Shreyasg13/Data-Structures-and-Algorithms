821572dd46026363a09d446e505e7cb55855ca75
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-08-11 14:30:54 -0400

    Time: 3557 ms (30.21%), Space: 59.2 MB (70.32%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
@@ -1,45 +1,12 @@
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
-        max_triplet = [1, 1, 1]
-        for triplet in triplets:
-            if triplet[0] <= target[0] and triplet[1] <= target[1] and triplet[2] <= target[2]:
-                max_triplet[0] = max(max_triplet[0], triplet[0])
-                max_triplet[1] = max(max_triplet[1], triplet[1])
-                max_triplet[2] =max(max_triplet[2], triplet[2])
-            if max_triplet == target:
+        Max=[1,1,1]
+        for t in triplets:
+            if t[0] <=target[0] and t[1] <= target[1] and t[2] <= target[2] :
+                Max[0]=max(Max[0],t[0])
+                Max[1]=max(Max[1],t[1])
+                Max[2]=max(Max[2],t[2])
+            if Max==target:
                 return True
         return False
             
-            
-                
-        
-#         if target in triplets:
-#             return True
-        
-#         def operation(a,b):
-#             ans=[]
-#             for i in range(len(triplets[0])):
-                
-#                 ans.append(max(a[i],b[i])) 
-#             return ans
-
-#         val=[] 
-#         for i in range(len(triplets)):
-#             for j in range(len(triplets),i+1):
-#                 val=operation(triplets[i],triplets[j])
-#                 if val == target:
-#                     return True
-                
-#             top=triplets.pop()
-#             triplets.append(val)
-            
-#         return False
-                    
-                    
-    
-                
-                
-                
-            
-        
-       
