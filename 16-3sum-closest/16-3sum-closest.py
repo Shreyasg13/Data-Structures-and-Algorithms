60ff77071fc4d4ab8c23eafd62bbea07657ab737
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-08-23 14:14:19 -0400

    Time: 9161 ms (18.32%), Space: 14.1 MB (57.66%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -2,21 +2,24 @@ class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         nums.sort()
         res=nums[0]+nums[1]+nums[2]
+        
         for i in range(len(nums)-2):
   
             l,r=i+1,len(nums)-1
             
             while l < r:
                 s=nums[l]+nums[r]+nums[i]
-                
+            # if close sum found the store in in res               
                 if abs(s-target) < abs(res-target):
                     res=s
+            # shift poiters on the basis of current sum        
                 if s > target:
                     r-=1
                 elif s < target:
                     l+=1
+            # break early in taget found
                 else:
-                    return res
+                     return res 
         return res        
        
        
