7525b99dc87b7be3adf994c654cf0c7aa0717579
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-08-23 14:36:36 -0400

    Time: 1679 ms (37.23%), Space: 17.9 MB (84.70%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -25,31 +25,3 @@ class Solution:
                             
                     
         return res
-    
-    
-    
-# class Solution:
-#     def threeSum(self, nums: List[int]) -> List[List[int]]:
-#         triplet=[]
-#         nums.sort()
-#         for i, ele in enumerate(nums):
-#             if i > 0 and ele==nums[i-1]:
-#                 continue
-            
-#             l,r=i+1,len(nums)-1
-#             while l < r:
-#                 Sum=nums[l]+nums[r]+ele
-            
-#                 if  Sum > 0:
-#                     r-=1
-#                 elif Sum < 0:
-#                     l+=1
-#                 else:
-#                     triplet.append([ele,nums[l],nums[r]])
-#                     l+=1
-            
-#                     while nums[l]==nums[l-1] and l < r  :
-#                         l+=1
-                        
-                        
-#         return triplet
