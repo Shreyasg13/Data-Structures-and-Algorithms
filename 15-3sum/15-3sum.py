39aebc1e4483ad41515958a52d12c627076883d0
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-08-23 14:34:58 -0400

    Time: 7136 ms (7.44%), Space: 18.2 MB (40.35%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,28 +1,56 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        triplet=[]
         nums.sort()
-        for i, ele in enumerate(nums):
-            if i > 0 and ele==nums[i-1]:
+        res=[]
+        for i in range(len(nums)-2):
+            
+            if i > 0 and nums[i]==nums[i-1]:
                 continue
             
             l,r=i+1,len(nums)-1
+            mid=(l+r)//2
             while l < r:
-                Sum=nums[l]+nums[r]+ele
             
-                if  Sum > 0:
+                sum=nums[i]+nums[l]+nums[r]
+            
+                if sum > 0:
                     r-=1
-                elif Sum < 0:
+                elif sum < 0:
                     l+=1
                 else:
-                    triplet.append([ele,nums[l],nums[r]])
-                    l+=1
-            
-                    while nums[l]==nums[l-1] and l < r  :
+                    triplet=[nums[i],nums[l],nums[r]]
+                    if triplet not in res:
+                        res.append(triplet)
                         l+=1
-                        
-                        
-        return triplet 
+                        while l < r and nums[l]==nums[l-1]:
+                            l+=1
+                    
+        return res
+    
+    
+    
+# class Solution:
+#     def threeSum(self, nums: List[int]) -> List[List[int]]:
+#         triplet=[]
+#         nums.sort()
+#         for i, ele in enumerate(nums):
+#             if i > 0 and ele==nums[i-1]:
+#                 continue
             
-                
-        
+#             l,r=i+1,len(nums)-1
+#             while l < r:
+#                 Sum=nums[l]+nums[r]+ele
+            
+#                 if  Sum > 0:
+#                     r-=1
+#                 elif Sum < 0:
+#                     l+=1
+#                 else:
+#                     triplet.append([ele,nums[l],nums[r]])
+#                     l+=1
+            
+#                     while nums[l]==nums[l-1] and l < r  :
+#                         l+=1
+                        
+                        
+#         return triplet
