096657e9c0f79242e1b7ec249ce8e0e4172b7ad4
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-08-25 15:57:10 -0400

    Time: 120 ms (43.89%), Space: 14.1 MB (32.89%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
@@ -1,10 +1,12 @@
 class Solution:
     def sortByBits(self, array: List[int]) -> List[int]:
-        def helper(d):
+        # helping function to calculate number of ones
+        def Weight(d):
             count=0
             d = str(bin(d))
             for n in d:
                 if n=="1":
                     count+=1
             return count
-        return sorted(array, key=lambda x: (helper(x), x))
+        # sort an array by the weight on ones i.e. key
+        return sorted(array, key=lambda x: (Weight(x), x))
