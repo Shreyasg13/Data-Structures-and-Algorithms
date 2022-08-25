c9c0a31f4fade73003958001630495c90a400988
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-08-25 18:22:17 -0400

    Time: 311 ms (5.04%), Space: 15.3 MB (12.59%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.py
@@ -7,6 +7,7 @@
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         
+        # Helping function to check if Subtree is same as current root tree
         def isSameTree(p,q):
             if not p and not q:
                 return True
@@ -14,12 +15,14 @@ class Solution:
                 return False            
             if p and q:
                 return p.val==q.val and isSameTree(p.left,q.left) and isSameTree(p.right,q.right)
-            
+        
+        # if it reaches end i.e. it could not found and same tree
         if not root:
                 return False
+        # if current root and subtree are same
         if isSameTree(root,subRoot) :
                 return True
-            
+        # keep recursing function on left and right side of tree    
         return self.isSubtree(root.left,subRoot) or self.isSubtree(root.right,subRoot)  
         
         
