b59b3f8e96f3f57b810b338df51bbf911ad5e2b2
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-08-25 17:39:54 -0400

    Time: 95 ms (17.53%), Space: 16.4 MB (42.64%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
@@ -10,53 +10,13 @@ class Solution:
         self.diameter=0
         
         def DFS(root):
-            if not root:
-                return 0
+            if not root:return 0
+            
             l,r=DFS(root.left),DFS(root.right)
             self.diameter=max(l+r,self.diameter)
+            print(l,r,self.diameter)
             return 1+max(l,r)
         
         DFS(root)
         return self.diameter
         
-        
-        
-        
-        
-        
-        
-#         self.res=0
-        
-    
-#         def Depth(root):
-#             if not root:
-#                 return 0
-#             l=Depth(root.left) 
-#             r=Depth(root.right)
-#             self.res=max(l+r,self.res)
-#             return 1+max(l,r)
-        
-#         Depth(root)
-#         return self.res
-        
-        
-    
-    
-    
-    
-#     class Solution(object):
-#     def diameterOfBinaryTree(self, root):
-#         """
-#         :type root: TreeNode
-#         :rtype: int
-#         """
-#         self.ans = 0
-        
-#         def depth(p):
-#             if not p: return 0
-#             left, right = depth(p.left), depth(p.right)
-#             self.ans = max(self.ans, left+right)
-#             return 1 + max(left, right)
-            
-#         depth(root)
-#         return self.ans
