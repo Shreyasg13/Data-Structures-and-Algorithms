eed9da8ded1baf960058be361ccf8b0f5f66aad6
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-08-25 20:26:24 -0400

    Time: 62 ms (24.71%), Space: 13.9 MB (70.69%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -6,19 +6,34 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        res=[]
         stack=[]
+        res=[]
         if root:
             stack.append(root)
+        
         while stack:
             level=[]
             for i in range(len(stack)):
+                # keep poping first node and append it to current level
                 node=stack.pop(0)
                 level.append(node.val)
-                
+                # keep storing left and right child in stack
                 if node.right   :  stack.append(node.right)
                 if node.left    :  stack.append(node.left)
-            
+            # after each level form keep appending that node in res
             res.append(level[0])
             
         return res
+        
+#         res, stack= [], [root] 
+#         if not root:
+#             return []
+#         while stack:
+#             res.append(stack[-1].val) # appending right most value
+#             level, stack = stack, []
+#             for node in level: # build the next level
+#                 if node.left: stack.append(node.left)
+#                 if node.right: stack.append(node.right)
+                    
+#         return res
+                
