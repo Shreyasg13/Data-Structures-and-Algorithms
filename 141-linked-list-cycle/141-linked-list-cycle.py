2ba345cb5a63eabaa6eedf4d131ebdf5ebb1df1f
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-08-26 16:48:22 -0400

    Time: 80 ms (61.21%), Space: 17.6 MB (67.21%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -6,15 +6,13 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        
-        fast,slow=head,head
+        slow,fast=head,head
         
         while fast and fast.next:
-            fast=fast.next.next
             slow=slow.next
+            fast=fast.next.next
             
-            if fast==slow:
+            if slow==fast:
                 return True
-            
         return False
-        
+    
