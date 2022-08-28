7d79b2bc6afcc172af66cbee9e1576547266b3f0
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-08-28 19:37:47 -0400

    Time: 115 ms (10.50%), Space: 14 MB (48.06%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -1,33 +1,27 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        
         rows,cols=len(grid),len(grid[0])
         fresh=set()
         rotten=collections.deque()
         
         for r in range(rows):
             for c in range(cols):
-                # Fresh
-                if grid[r][c]==1 :
+                # fresh oranges
+                if grid[r][c]==1:
                     fresh.add((r,c))
-                # Rotten
-                elif grid[r][c]==2:
+                # rotten store in deque
+                if grid[r][c]==2:
                     rotten.append((r,c))
+                 
         time=0
-        
         while fresh and rotten:
-            # BFS iteration
-            for i in range(len(rotten)):
+            # print(fresh,rotten)
+            for _ in range(len(rotten)):
                 r,c=rotten.popleft()
-                for adj in ((r+1,c),(r-1,c),(r,c+1),(r,c-1)):
+                # check for adjucent oranges
+                for adj in ((r,c+1),(r,c-1),(r+1,c),(r-1,c)):
                     if adj in fresh:
                         fresh.remove(adj)
                         rotten.append(adj)
             time+=1
-            
         return -1 if fresh else time
-        
-                
-        
-        
-        
