3d5d0bff1c8250ad7a939033ea669c38def5e8b6
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-08-28 22:45:14 -0400

    Time: 391 ms (52.23%), Space: 32.8 MB (7.68%) - LeetHub

---
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        
+        q=collections.deque()
+        q.append((root,float('-inf'))) #q((root,max_value))
+        res=0
+        
+        """
+            1.poping node and max_value
+            2.keep on checking if its good node or node
+            3.push nodes in q if left or right child exist with updated max values  
+        T(C):
+            
+        """
+        while q:
+
+            root,max_val=q.popleft() 
+            
+            if max_val <=root.val:
+                res+=1
+            
+            if root.left:
+                q.append((root.left,max(root.val,max_val)))
+            if root.right:
+                q.append((root.right,max(root.val,max_val)))
+        
+        return res
