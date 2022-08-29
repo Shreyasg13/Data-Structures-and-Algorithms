6b3b5dfb0d1315be531bc602f23249809507bbf6
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-08-29 13:58:48 -0400

    Time: 163 ms (47.10%), Space: 16.7 MB (61.21%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,50 +1,39 @@
 class Solution:
     def canFinish(self, N, edges):
-        Adj_List = [[] for _ in range(N)]
-        status = [0 for _ in range(N)]
-        for x,y in edges: Adj_List[x].append(y)
+        # Create a map of crs and their repective prereqs
+        adj_list=[[] for i in range(N)]
+        for x,y in edges: adj_list[x].append(y)
         
-        def dfs(crs):
-            # if came across visited course
-            if status[crs]==1:
-                return True
-            # already visited course
-            if status[crs]==-1:
+        
+        """
+    # Implement DFS for each course
+        1.Maintain the visit set
+        2. if the course you selected 
+            I.add crs in visit set and check for its prereqs
+            II. Its in visited set i.e. you found a cycle
+            III. If its empty i.e. its prerequisites are taken already and can truen True
+        3. call the DFS function on all number of courses
+        """
+        visit=set()
+        def DFS(crs):
+            
+            if crs in visit:
                 return False
-            status[crs]= -1
+            if adj_list[crs]==[]:
+                return True
             
-            for pre in Adj_List[crs]:
+            visit.add(crs)
+            for nei in adj_list[crs]:
                 
-                if not dfs(pre):
+                if not DFS(nei):
                     return False
-            
-            status[crs]=1
+            visit.remove(crs)
+            adj_list[crs]=[]
             return True
-        
+            
         for crs in range(N):
-            if not dfs(crs):
+            if not DFS(crs):
                 return False
         return True
             
-            
-                
-        
-            
-        
-        
-#         pre = defaultdict(list)
-#         for x, y in edges: pre[x].append(y)
-
-#         status = [0] * N
-#         def canTake(i):
-#             if status[i] in {1, -1}: return status[i] == 1
-#             status[i] = -1
-#             if any(not canTake(j) for j in pre[i]): return False
-#             status[i] = 1
-#             return True
-
-#         return all(canTake(i) for i in range(N))
-
-
-        
         
