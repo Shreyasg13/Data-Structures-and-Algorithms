a1570acc924bcb826935a1db7d558638a6848d2d
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-08-30 12:45:32 -0400

    Time: 79 ms (5.47%), Space: 13.9 MB (71.22%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -1,9 +1,10 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        return max( nums[0],self.rob_with_linear(nums[1:]) , self.rob_with_linear(nums[0:-1]) )
         
-    def rob_with_linear(self,amount):
-        fir,sec=0,0
-        for i in range(len(amount)):
-            fir,sec=sec,max(fir+amount[i],sec)
-        return sec
+        def rob_linear(nums):
+            f,s=0,0
+            for i in range(len(nums)):
+                s,f=max(nums[i]+f,s),s
+            return s
+        print(nums[0:-1])
+        return max(rob_linear(nums[1:]),rob_linear(nums[0:-1]),nums[0])
