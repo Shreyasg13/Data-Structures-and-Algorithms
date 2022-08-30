76ad3523d100040f075414b5229c83ee16b3a088
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-08-29 22:42:59 -0400

    Time: 37 ms (78.83%), Space: 13.9 MB (57.02%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -3,8 +3,8 @@ class Solution:
         # top down approach
         last,prev=1,1
         
-        for i in range(n-1):
-            # change the last to previous and simply keep updating previou \
+        while n-1 > 0:
             last,prev=prev,prev+last
-        return prev
+            n-=1
         
+        return prev
