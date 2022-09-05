3871c21389c9a6974a8c2f9fac339955d11dadb5
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-09-05 17:48:21 -0400

    Time: 70 ms (16.50%), Space: 14.1 MB (72.17%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
@@ -6,17 +6,13 @@
 #         self.right = right
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        level,q=[],deque()
-        if root:
-            q.append((root,0))
-        else:
-            return []
-        Map=defaultdict(list)
+        q=deque()
+        Map=defaultdict(list) #{None:[]}
         
+        q.append((root,0))
         while q:
             level=[]
             for i in range(len(q)):
-                
                 node,x_axis=q.popleft()
                 level.append([node.val,x_axis])
                 
@@ -24,17 +20,13 @@ class Solution:
                     q.append( (node.right , x_axis+1) )
                 if node.left:
                     q.append( (node.left , x_axis-1) )
-                
+                    
+            # feed the sorted order in level to maintain ascending sequence in nth level    
             for n,x in sorted(level):
-                Map[x].append(n)    
-            
-            print(Map)
+                Map[x].append(n)
         res=[]
-        
         for key in sorted(Map.keys()):
             res.append(Map[key])
-      
-        
         return res
        
             
