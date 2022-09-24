aad940a7e07b648c0bcae9ff72b51d34d7f392c5
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-09-24 01:11:00 -0400

    Time: 1801 ms (14.95%), Space: 19 MB (27.07%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.py
+++ 997-find-the-town-judge/997-find-the-town-judge.py
@@ -1,26 +1,12 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        
-        if n==0: return -1
-        Map=defaultdict(set)
-        
-        for i in range(1,n+1):
-            Map[i] = set()
+        trusted=[0]*(n+1)
         
         for x,y in trust:
-            Map[x].add(y)
-        
-        # person having no edges  
-        
-        person = [i for i,v in Map.items() if len(v) == 0]
-        
-        if len(person) == 0: return -1
-        person = person[0]
-        
-        # go through edges and see if everybody trusts candidate
-        for i,v in Map.items():
-            
-            if person == i: continue
-            if person not in Map[i]:
-                return -1
-        return person
+            trusted[x]-=1
+            trusted[y]+=1
+        for i in range(1,n+1):
+            if trusted[i]==n-1:
+                return i
+        return -1
+ 
