13453cf712b164bb2ee1be2826dfc0dde10f5d46
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-10-05 15:53:13 -0400

    Time: 437 ms (68.56%), Space: 15.9 MB (64.02%) - LeetHub

--- 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.py
+++ 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.py
@@ -1,27 +1,30 @@
 class Solution:
     def ways(self, grid, k):
-        n, m = len(grid), len(grid[0])
-        N, M = n + 1, m + 1
-        sum_grid = [[0] * M for _ in range(N)]
-        for i in range(1, N):
-            for j in range(1, M):
-                sum_grid[i][j] = sum_grid[i-1][j] + sum_grid[i][j-1]- sum_grid[i-1][j-1] + int(grid[i-1][j-1] == 'A')
-
         dp = {}
         def count(a, b, c, d):
-            return sum_grid[c][d] - sum_grid[a][d] - sum_grid[c][b] + sum_grid[a][b]
+            return total[c][d] - total[a][d] - total[c][b] + total[a][b]
+            
         def dfs(a, b, k):
             if (a, b, k) in dp: return dp[(a, b, k)]
-            if k == 1 and count(a, b, n, m) >= 1: return 1
+            if k == 1 and count(a, b, row, col) >= 1: return 1
 
             ret, mod = 0, int(1e9 + 7)
-            for i in range(a + 1, n):
-                if count(a, b, i, m) >= 1:
+            for i in range(a + 1, row):
+                if count(a, b, i, col) >= 1:
                     ret = (ret + dfs(i, b, k - 1)) % mod
-            for j in range(b + 1, m):
-                if count(a, b, n, j) >= 1:
+            for j in range(b + 1, col):
+                if count(a, b, row, j) >= 1:
                     ret = (ret + dfs(a, j, k - 1)) % mod
             dp[(a, b, k)] = ret
             return ret
+            
+        row, col = len(grid), len(grid[0])
+        x, y = row + 1, col + 1
+        total = [[0] * y for _ in range(x)]
+        for i in range(1, x):
+            for j in range(1, y):
+                total[i][j] = total[i-1][j] + total[i][j-1]- total[i-1][j-1] + int(grid[i-1][j-1] == 'A')
 
+        
+        
         return dfs(0, 0, k)
