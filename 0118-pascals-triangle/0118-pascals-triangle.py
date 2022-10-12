306fa6d1fc18e007749fac00a218bad1229a446d
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-10-12 13:35:32 -0400

    Time: 66 ms (14.12%), Space: 14 MB (17.76%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -5,9 +5,13 @@ class Solution:
         i=0
         
         while i < numRows-1 :
+            # initial border 1
             layer=[1]
+            # addition of in between values
             for j in range(1,len(res[i])):
                 layer.append(res[i][j]+res[i][j-1])
+            
+            # end border 1
             layer.append(1)
             res.append(layer)
             i+=1
