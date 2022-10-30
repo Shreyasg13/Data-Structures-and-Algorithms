6fc7dbba0de81fc537d24473d7ca45de951cda64
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-10-30 16:42:04 -0400

    Time: 399 ms (26.17%), Space: 15.3 MB (48.90%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -4,8 +4,9 @@ class Solution:
         leftsum=0
         
         for i in range(len(nums)):
-            
+            # checking if leftsum is equals rightsum
             if leftsum==total-nums[i]-leftsum:
                 return i
+            # update leftsum at index i
             leftsum+=nums[i]
         return -1
