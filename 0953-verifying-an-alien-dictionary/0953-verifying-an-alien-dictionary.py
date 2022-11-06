fd58034552399376a60bbbeb0182c54a171b1b03
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-11-06 15:11:33 -0500

    Time: 40 ms (90.97%), Space: 14 MB (39.59%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -9,13 +9,13 @@ class Solution:
             map[key]=val
         print(map)
         """
-        
+        # 2. compare two words character by character
         for i in range(len(words)-1):
             for j in range(len(words[i])):
-                
+                # for case like apple and app
                 if j >= len(words[i+1]):
                     return False
-                
+                # check if they are sorted if not equal
                 if words[i][j] != words[i+1][j]:
                     if map[words[i][j]] > map[words[i+1][j]] :
                         return False
