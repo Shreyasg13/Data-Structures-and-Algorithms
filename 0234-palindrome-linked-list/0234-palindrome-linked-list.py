dc6a48fb2370d1c31c7fa026b8fb2e57a8dadf48
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-11-06 11:38:42 -0500

    Time: 1886 ms (40.01%), Space: 39 MB (78.60%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -9,9 +9,10 @@ class Solution:
         sec=head
         # Find middle
         while sec and sec.next:
-            
+            sec=sec.next
+            sec=sec.next
             fir=fir.next
-            sec=sec.next.next
+            
             
         prev=None
         # reverse next half
@@ -30,4 +31,52 @@ class Solution:
             right=right.next
         return True
             
- 
+            
+        
+#             node2=head
+#             node=head
+            
+#             def rev(node):
+#                 prev,cur=None,node
+                
+#                 while cur:
+#                     nxt=cur.next
+#                     cur.next=prev
+#                     prev=cur
+#                     cur=nxt
+#                 return prev
+#             head2=rev(node)
+#             print(head)
+#             while head and head2 :
+#                 if head.val != head2.val:
+#                     return False
+                
+#                 return True
+            
+    
+#         first=second=node=head
+#         l1=0
+#         while first and second:
+#             l1+=1
+#             first=first.next
+#             second=second.next.next
+            
+#         tmp=mid=first
+#         print()
+#         first.next=None
+        
+#         while tmp :
+#             tmp=tmp.next
+#             l1-=1
+#         if l1 !=0 :
+#             mid=mid.next
+#         print(mid,first)
+#         return mid==first
+            
+        
+        
+        
+        
+            
+            
+        
