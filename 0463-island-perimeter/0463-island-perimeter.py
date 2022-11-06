3959b4eec243d0dba48e28a186ac4809e310cd0c
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-11-06 13:23:46 -0500

    Time: 1586 ms (28.33%), Space: 27.3 MB (15.27%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,19 +1,19 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         visit=set()
-        # res=0
+        
         def DFS(i,j):
+            # bound conditions : if edges found
             if i < 0 or i >= len(grid) or j < 0 or j >=len(grid[0]) or grid[i][j]==0:
                 return 1
-            
+            # IF already visited cell found
             if (i,j) in visit : 
                 return 0
             
             visit.add((i,j))
-            
-            
             return DFS(i+1,j)+DFS(i-1,j)+DFS(i,j+1)+DFS(i,j-1)
         
+        # Iterating through each and calling DFS on 1s
         for r in range(len(grid)):
             for c in range(len(grid[0])):
                 if grid[r][c]:
