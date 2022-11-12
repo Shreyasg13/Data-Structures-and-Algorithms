63597e61819d3810206de5e3c26db7f8e8918df2
Shreyash Gondane <103242665+Shreyasg13@users.noreply.github.com>
2022-11-11 21:37:58 -0500

    Time: 64 ms (90.45%), Space: 14 MB (75.11%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+         # revesing the array start at 10 
+            # [10,15,20]
+#         for i in range(len(cost)-3,-1,-1):
+#             # checking fist step and second step min
+#             cost[i]+=min(cost[i+1],cost[i+2])  
+#         return min(cost[0],cost[1])
+        
+        for i in range(len(cost)-3,-1,-1):
+            cost[i]+=min(cost[i+1],cost[i+2])
+        
+        return min(cost[0],cost[1])
